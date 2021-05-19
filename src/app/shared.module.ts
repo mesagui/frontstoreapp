2373339e0ea3ce9ee651e9929f4b6cb097f52856
Mike Estrada <mg.estrada.a@gmail.com>
2021-05-19 12:08:41 -0500

    Recien Creado

---
+++ src/app/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModalModule.forRoot()
+  ]
+})
+export class SharedModule { }
